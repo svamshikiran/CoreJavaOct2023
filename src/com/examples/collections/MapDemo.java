479845a567221fc504daea3b052ce1b3764b625b
Vamshi Singireddy <svamshikiran@gmail.com>
2023-11-01 07:59:01 +0530

    Modified the name of the student in MapDemo.java

--- src/com/examples/collections/MapDemo.java
+++ src/com/examples/collections/MapDemo.java
@@ -10,14 +10,10 @@ public class MapDemo {
 
         studentRollnoMap.put(1, "Java");
         studentRollnoMap.put(2, "Spring");
-        studentRollnoMap.put(3, "Sql");
+        studentRollnoMap.put(3, "PostgreSQL");
         studentRollnoMap.put(4, "Maven");
         studentRollnoMap.put(5, "Database");
         studentRollnoMap.put(6, "Git");
-
-        studentRollnoMap.put(2, "Springboot");
-
-        System.out.println("VALUE OF KEY 2: "+studentRollnoMap.get(2));
 
         Set<Integer> keySet = studentRollnoMap.keySet();
 
